9c490f1dfb621079a69f344536e675606a93861b
thecodecoffeelab <devsyndicate@proton.me>
2023-04-08 14:27:49 +0000

    mohamed globe view

---
+++ src/world/components/lights.js
@@ -0,0 +1,19 @@
+import { AmbientLight, DirectionalLight, PointLight } from "three";
+
+function createLights() {
+  const ambientLight = new AmbientLight(0x509bff, 0.3);
+
+  const dLight = new DirectionalLight(0xffffff, 0.7);
+  dLight.position.set(-400, 100, 400);
+
+  const dLight1 = new DirectionalLight(0x455dee, 1);
+  dLight1.position.set(-200, 500, 200);
+
+  const dLight2 = new PointLight(0x13176d, 0.8);
+  dLight2.position.set(-200, 500, 200);
+
+  return { ambientLight, dLight, dLight1, dLight2 };
+}
+
+export { createLights };
+
